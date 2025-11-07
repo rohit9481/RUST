a6991b273bc005370a66ff2e723bff3d94ea58f6
Rohit Yakkundi <111986558+rohit9481@users.noreply.github.com>
2025-11-07 19:46:17 +0530

    Refactor main function and add print statements
    Refactored main function to use print module and added various print statements.

--- RUST_P/src/main.rs
+++ RUST_P/src/main.rs
@@ -1,3 +1,12 @@
+mod print;
 fn main() {
-    println!("Hello, world!");
+    print::run();
+    println!("numbers {}", 1);
+    println!("{} is from {}", "bread","Mass");
+    println!("{0} is from {1} and {0} likes {2}", "bread","Mass","code");
+    println!("{name} likes to play {game}", name="Alice", game="chess");
+    println!("Binary: {:b}, Hex: {:x}, Octal: {:o}", 10, 10, 10);
+    println!("{:?}",(12,true,"Hello"));
+    println!("10 + 10 = {}", 10+10);
+
 }
