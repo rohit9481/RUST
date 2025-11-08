506803d2f9f3fc9d0a8f39d6724059e8382c36cf
Rohit Yakkundi <111986558+rohit9481@users.noreply.github.com>
2025-11-08 13:01:49 +0530

    Add files via upload

---
+++ RUST_P/src/loops.rs
@@ -0,0 +1,26 @@
+pub fn run(){
+    let mut count = 0;
+
+    // loop{
+    //     count +=1;
+    //     println!("count:{}",count);
+    //     if count == 20{
+    //         break;
+    //     }
+    // }
+
+    //while
+
+    while count <=100{
+        if count % 15 == 0{
+            println!("fizzbuzz");
+        }else if count % 3 == 0{
+            println!("fizz");
+        }else if count % 5 == 0{
+            println!("buzz");
+        }else{
+            println!("{}",count);
+        }
+        count +=1;
+    }
+}
