2686cf6ebc069b7378bcc91095840304c020738c
Rohit Yakkundi <111986558+rohit9481@users.noreply.github.com>
2025-11-08 12:47:28 +0530

    Refactor main.rs by commenting out code
    Comment out various modules and print statements.

--- RUST_P/src/main.rs
+++ RUST_P/src/main.rs
@@ -1,12 +1,30 @@
-mod print;
+// mod print;
+// mod var;
+//mod data_type;
+//mod strings;
+// mod tuples;
+// mod arrays;
+mod vector;
+
 fn main() {
-    print::run();
-    println!("numbers {}", 1);
-    println!("{} is from {}", "bread","Mass");
-    println!("{0} is from {1} and {0} likes {2}", "bread","Mass","code");
-    println!("{name} likes to play {game}", name="Alice", game="chess");
-    println!("Binary: {:b}, Hex: {:x}, Octal: {:o}", 10, 10, 10);
-    println!("{:?}",(12,true,"Hello"));
-    println!("10 + 10 = {}", 10+10);
+    vector::run();
+    //arrays::run();
+    
+    //strings::run();
+    
+    // tuples::run();
+
+    //data_type::new();
+
+    //var::run();
+
+    // print::run();
+    // println!("numbers {}", 1);
+    // println!("{} is from {}", "bread","Mass");
+    // println!("{0} is from {1} and {0} likes {2}", "bread","Mass","code");
+    // println!("{name} likes to play {game}", name="Alice", game="chess");
+    // println!("Binary: {:b}, Hex: {:x}, Octal: {:o}", 10, 10, 10);
+    // println!("{:?}",(12,true,"Hello"));
+    // println!("10 + 10 = {}", 10+10);
 
 }
